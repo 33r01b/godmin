4fc2bafb4d886346cf053c514c77742fd6ffbde4
zeroonebeatz <zeroonebeatz@gmail.com>
2021-02-03 02:20:06 +0300

    feat: add main page

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module godmin
 go 1.15
 
 require (
+	github.com/google/uuid v1.2.0
+	github.com/gorilla/mux v1.8.0
 	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jackc/pgx/v4 v4.10.1 // indirect
 	github.com/jmoiron/sqlx v1.3.1
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,10 +2,22 @@ package server
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 	"godmin/config"
 	"godmin/internal/store"
+	"net/http"
+	"time"
 )
+
+const (
+	ctxKeyUser      ctxKey = iota
+	ctxKeyRequestID ctxKey = iota
+)
+
+type ctxKey int8
 
 func Run(context context.Context, config *config.Config) error {
 	log.SetLevel(config.LogLevel)
@@ -16,5 +28,81 @@ func Run(context context.Context, config *config.Config) error {
 	}
 	defer db.Close()
 
-	return nil
+	return http.ListenAndServe(config.BindAddr, newServer(store.New(db)))
+}
+
+func newServer(store *store.Store) *server {
+	s := &server{
+		router: mux.NewRouter(),
+		store:  store,
+	}
+
+	s.configureRouter()
+
+	return s
+}
+
+type server struct {
+	router *mux.Router
+	store  *store.Store
+}
+
+func (s *server) configureRouter() {
+	s.router.Use(s.setRequestID)
+	s.router.Use(s.logRequest)
+	// TODO
+	s.router.HandleFunc("/", s.handleMain()).Methods(http.MethodGet)
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.router.ServeHTTP(w, r)
+}
+
+func (s *server) setRequestID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := uuid.New().String()
+		w.Header().Set("X-Request-ID", id)
+		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), ctxKeyRequestID, id)))
+	})
+}
+
+func (s *server) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger := log.WithFields(log.Fields{
+			"remote_addr": r.RemoteAddr,
+			"request_id":  r.Context().Value(ctxKeyRequestID),
+		})
+		logger.Infof("started %s %s", r.Method, r.RequestURI)
+
+		start := time.Now()
+		rw := &responseWriter{w, http.StatusOK}
+		next.ServeHTTP(rw, r)
+
+		logger.Infof(
+			"completed with %d %s in %v",
+			rw.code,
+			http.StatusText(rw.code),
+			time.Now().Sub(start),
+		)
+	})
+}
+
+func (s *server) handleMain() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		s.respond(w, r, http.StatusOK, "hello")
+	}
+}
+
+func (s *server) respond(w http.ResponseWriter, r *http.Request, code int, data interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	if data != nil {
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			s.error(w, r, http.StatusInternalServerError, err)
+		}
+	}
+}
+
+func (s *server) error(w http.ResponseWriter, r *http.Request, code int, err error) {
+	s.respond(w, r, code, map[string]string{"error": err.Error()})
 }
